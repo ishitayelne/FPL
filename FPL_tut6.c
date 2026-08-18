7b00b2072d401476313b0fa8d2a3c1995452ec4b
Ishita Yelne <ishitayelne27@gmail.com>
2026-08-18 23:32:44 +0530

    Create FPL_tut6.c

--- FPL_tut6.c
+++ FPL_tut6.c
@@ -10,12 +10,12 @@ int swap(int *x,int *y) {
 int main() {
      int a,b;
     printf("Enter 2 numbers: ");
-    scanf("%d%d",&a,&b);
-    printf("First Number before swap %d\n",a);
-    printf("Second Number before swap %d\n",b);
-    swap(&a,&b);
-    printf("First Number after swap %d\n",a);
-    printf("Second Number after swap %d\n",b);
+    scanf("%d%d", &a, &b);
+    printf("First Number before swap %d\n", a);
+    printf("Second Number before swap %d\n", b);
+    swap(&a, &b);
+    printf("First Number after swap %d\n", a);
+    printf("Second Number after swap %d\n", b);
 
     return 0;
 }
